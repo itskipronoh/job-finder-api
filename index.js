4b8cc6c88fbd9835f2b916d85c6afe578b20c1fc
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-16 19:08:32 +0300

    fix access initialization
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Job = require('./models/JobModel')
+const Job = require('./models/JobModel');
 const app = express()
 
 app.use(express.json())
@@ -18,67 +18,58 @@ app.get('/blog', (req, res) => {
 
 app.get('/Jobs', async(req, res) => {
     try {
-        const Jobs = await Job.find({});
-        res.status(200).json(Jobs);
+        const jobs = await Job.find({});
+        res.status(200).json(jobs);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
-})
+});
 
-app.get('/Jobs/:id', async(req, res) =>{
+app.get('/Jobs/:id', async(req, res) => {
     try {
-        const {id} = req.params;
-        const Job = await Job.findById(id);
-        res.status(200).json(Job);
+        const { id } = req.params;
+        const job = await Job.findById(id);
+        res.status(200).json(job);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
-})
-
+});
 
 app.post('/Jobs', async(req, res) => {
     try {
-        const Job = await Job.create(req.body)
-        res.status(200).json(Job);
-        
+        const job = await Job.create(req.body);
+        res.status(200).json(job);
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
-})
+});
 
-// update a job
 app.put('/Jobs/:id', async(req, res) => {
     try {
-        const {id} = req.params;
-        const Job = await Job.findByIdAndUpdate(id, req.body);
-        // cannot find any job in database
-        if(!Job){
-            return res.status(404).json({message: `cannot find any Job with ID ${id}`})
+        const { id } = req.params;
+        const job = await Job.findByIdAndUpdate(id, req.body, { new: true });
+        if (!job) {
+            return res.status(404).json({message: `Cannot find any Job with ID ${id}`});
         }
-        const updatedJob = await Job.findById(id);
-        res.status(200).json(updatedJob);
-        
+        res.status(200).json(job);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
-})
+});
 
-// delete a job
-
-app.delete('/Jobs/:id', async(req, res) =>{
+app.delete('/Jobs/:id', async(req, res) => {
     try {
-        const {id} = req.params;
-        const Job = await Job.findByIdAndDelete(id);
-        if(!Job){
-            return res.status(404).json({message: `cannot find any Job with ID ${id}`})
+        const { id } = req.params;
+        const job = await Job.findByIdAndDelete(id);
+        if (!job) {
+            return res.status(404).json({message: `Cannot find any Job with ID ${id}`});
         }
-        res.status(200).json(Job);
-        
+        res.status(200).json(job);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
-})
+});
 
 mongoose.set("strictQuery", false)
 mongoose.
@@ -87,7 +78,7 @@ connect('mongodb+srv://user:admin@cluster0.iz8twkd.mongodb.net/job-finder-api?re
     console.log('connected to MongoDB')
     app.listen(3000, ()=> {
         console.log(`Job Finder API app is running on port 3000`)
-    });
+    }); 
 }).catch((error) => {
     console.log(error)
 })
--- models/JobModel.js
+++ models/JobModel.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const JobSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Please enter a your name  name"]
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        // location: {
-        //     type: string,
-        //     required: true,
-        // },
-        email: {
-            type: String,
-            required: true,
-        }
+const jobSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter a your name  name"]
     },
-    {
-        timestamps: true
+    position: {
+        type: String,
+        required: [true, "Please enter a position name"]
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    
+    email: {
+        type: String,
+        required: true,
     }
+},
+{
+    timestamps: true
+}
 )
 
 
-const Job = mongoose.model('Job', JobSchema);
+const Job = mongoose.model('Job', jobSchema);
 
 module.exports = Job;
